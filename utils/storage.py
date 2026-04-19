3fb0530b1b70cded5a4e6245713584ae4e7f546d
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-04-19 16:28:53 -0500

    Update storage.py

--- utils/storage.py
+++ utils/storage.py
@@ -1,1 +1,27 @@
+import csv
+import os
 
+def load_existing(file_path):
+    existing = set()
+
+    if os.path.exists(file_path):
+        with open(file_path, "r", encoding="utf-8") as f:
+            reader = csv.reader(f)
+            next(reader, None)
+            for row in reader:
+                existing.add(row[1])
+
+    return existing
+
+
+def save_jobs(file_path, jobs):
+    file_exists = os.path.isfile(file_path)
+
+    with open(file_path, "a", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
+
+        if not file_exists:
+            writer.writerow(["empresa", "titulo"])
+
+        for job in jobs:
+            writer.writerow([job["company"], job["title"]])
